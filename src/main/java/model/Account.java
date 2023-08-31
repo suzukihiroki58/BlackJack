90cefeeae9f7e55dafc648a8caeae6f5739c6f4e
suzukihiroki58 <141290873+suzukihiroki58@users.noreply.github.com>
2023-08-31 18:00:31 +0900

    isUserRegisteredSuccessfullyメソッドの引数をAccountクラスに置き換え

--- src/main/java/model/Account.java
+++ src/main/java/model/Account.java
@@ -3,20 +3,26 @@ package model;
 public class Account {
 	private String userId;
 	private String userName;
-	private String hashedPassword; 
+	private String hashedPassword;
 	private String salt;
 	private String nickname;
 	private String role;
-
-
+	
 	public Account(String userId, String userName, String hashedPassword, String salt, String nickname, String role) {
 		this.userId = userId;
 		this.userName = userName;
 		this.hashedPassword = hashedPassword;
-        this.salt = salt;
+		this.salt = salt;
 		this.nickname = nickname;
 		this.role = role;
-		
+	}
+	
+	public Account(String userId, String userName, String hashedPassword, String salt, String nickname) {
+		this(userId, userName, hashedPassword, salt, nickname, null);
+	}
+
+	public Account(String userName, String hashedPassword, String nickname) {
+		this(null, userName, hashedPassword, null, nickname, null);
 	}
 
 	public String getUserId() {
@@ -27,35 +33,43 @@ public class Account {
 		this.userId = userId;
 	}
 
-	public String getUsername() {
+	public String getUserName() {
 		return userName;
 	}
 
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
 	public String getHashedPassword() {
-        return hashedPassword;
-    }
+		return hashedPassword;
+	}
 
-    public void setHashedPassword(String hashedPassword) {
-        this.hashedPassword = hashedPassword;
-    }
+	public void setHashedPassword(String hashedPassword) {
+		this.hashedPassword = hashedPassword;
+	}
 
-    public String getSalt() {
-        return salt;
-    }
+	public String getSalt() {
+		return salt;
+	}
 
-    public void setSalt(String salt) {
-        this.salt = salt;
-    }
+	public void setSalt(String salt) {
+		this.salt = salt;
+	}
 
 	public String getNickname() {
 		return nickname;
 	}
-	
+
+	public void setNickname(String nickname) {
+		this.nickname = nickname;
+	}
+
 	public String getRole() {
-		  return role;
-		}
+		return role;
+	}
 
 	public void setRole(String role) {
-	  this.role = role;
+		this.role = role;
 	}
 }
